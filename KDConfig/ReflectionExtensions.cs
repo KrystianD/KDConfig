6d6c4b1c60bb63826cab9f7f3ac0b5037484e7a5
Krystian Dużyński <krystian.duzynski@gmail.com>
2021-11-18 19:40:59 +0100

    added ReflectionExtensions

---
+++ KDConfig/ReflectionExtensions.cs
@@ -0,0 +1,22 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+
+namespace KDConfig
+{
+  public static class ReflectionExtensions
+  {
+    public static bool IsGenericList(this Type type)
+    {
+      return type.IsGenericType && type.GetGenericTypeDefinition() == typeof(List<>);
+    }
+  }
+
+  public static class ReflectionUtils
+  {
+    public static IList CreateListInstance(Type itemType)
+    {
+      return (IList)Activator.CreateInstance(typeof(List<>).MakeGenericType(itemType));
+    }
+  }
+}
